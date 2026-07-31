cc025dc279e78de78518204868d609a2b18f240b
Caputi <lcaputi2@gmail.com>
2026-07-31 13:57:37 -0300

    feat(db): implement status and teamEnum

---
+++ src/main/java/br/com/ubots/flowpay/model/enums/StatusEnum.java
@@ -0,0 +1,8 @@
+package br.com.ubots.flowpay.model.enums;
+
+public enum StatusEnum {
+    AGUARDANDO,
+    EM_ANDAMENTO,
+    FINALIZADA,
+    RECUSADA
+}
---
+++ src/main/java/br/com/ubots/flowpay/model/enums/TeamEnum.java
@@ -0,0 +1,7 @@
+package br.com.ubots.flowpay.model.enums;
+
+public enum TimeEnum {
+    CARTOES,
+    EMPRESTIMOS,
+    OUTROS_ASSUNTOS
+}
