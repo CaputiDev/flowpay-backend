f8b81ac0205a734617a934c62883bca67aa39ea7
Caputi <lcaputi2@gmail.com>
2026-08-04 14:23:09 -0300

    chore: fix hardcoded team max_capacity

--- src/main/java/br/com/ubots/flowpay/FlowpayApplication.java
+++ src/main/java/br/com/ubots/flowpay/FlowpayApplication.java
@@ -2,8 +2,10 @@ package br.com.ubots.flowpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class FlowpayApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/ubots/flowpay/repository/AgentRepository.java
+++ src/main/java/br/com/ubots/flowpay/repository/AgentRepository.java
@@ -4,6 +4,7 @@ import br.com.ubots.flowpay.model.Agent;
 import br.com.ubots.flowpay.model.enums.TeamEnum;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,9 +14,9 @@ import java.util.UUID;
 public interface AgentRepository extends CrudRepository<Agent, UUID> {
 
     /**
-     * Busca o primeiro atendente do time que possui menos de 3 chamados ativos,
+     * Busca o primeiro atendente do time que possui disponibilidade
      * priorizando quem tem a MENOR carga de trabalho atual (distribuição equilibrada).
      */
-    @Query("SELECT * FROM agent WHERE team = :team AND current_load < 3 ORDER BY current_load ASC LIMIT 1")
-    Optional<Agent> findAvailableAgentByTeam(TeamEnum team);
+    @Query("SELECT * FROM agent WHERE team = :team AND current_load < max_capacity ORDER BY current_load ASC LIMIT 1")
+    Optional<Agent> findAvailableAgentByTeam(@Param("team") TeamEnum team);
 }
